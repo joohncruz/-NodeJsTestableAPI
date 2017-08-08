b7407574a8d8fff344f557abff3847d92c7359b8
Jonathan Cruz Rocha Ferreira <joohncruzrocha@gmail.com>
2017-08-08 14:41:46 -0300

    Add project

---
+++ books-api/config/config.js
@@ -0,0 +1,14 @@
+export default {
+    database: 'books',
+    username: '',
+    password: '',
+    params: {
+        dialect: 'sqlite',
+        storage: 'books.sqlite',
+        define: {
+            underscored: true
+        }
+    }
+
+
+}
